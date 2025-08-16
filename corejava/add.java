039b04634d5632ac53d35145345c2254df09c88b
Teesha-Verma <teeshaverma1@gmail.com>
2025-08-16 15:28:46 +0530

    add

---
+++ corejava/add.java
@@ -0,0 +1,13 @@
+package bridgeLabz.corejava;
+import java.util.*;
+public class add {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.print(a+b);
+
+        sc.close();
+    }
+    
+}
