cb0f627ca8343f70038d7ccb422c6d2c8c7e0f17
Teesha <157734859+Teesha-Verma@users.noreply.github.com>
2025-08-19 11:26:16 +0530

    Update demo.java

--- oops/demo.java
+++ oops/demo.java
@@ -1,1 +1,18 @@
+package bridgeLabz.oops;
+class demo{
+    int var1;
+    demo(int var1){
+        this.var1=var1;
+    }
 
+    void xyz(){
+        int var1=90;
+        System.out.println(var1);
+        System.out.println(this.var1);
+    }
+
+    public static void main(String[] args){
+        demo obj=new demo(100);
+        obj.xyz();
+    }
+}
