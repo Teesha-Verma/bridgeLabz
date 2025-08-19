47d0c1c89878d520af2a222212223f16346f6ca0
Teesha <157734859+Teesha-Verma@users.noreply.github.com>
2025-08-19 11:06:52 +0530

    Update student.java

--- oops/student.java
+++ oops/student.java
@@ -1,1 +1,23 @@
+package bridgeLabz.oops;
+class student{
+    String name;
+    int marks;
 
+    student(String name, int marks){
+        this.name=name;
+        this.marks=marks;
+    }
+
+    void displayData(){
+        System.out.println("Name: "+name+" , Marks: "+marks);
+    }
+
+    public static void main (String[]args){
+        student s1=new student("abc", 89);
+        student s2=new student("xyz", 78);
+
+        s1.displayData();
+        s2.displayData();
+
+    }
+}
