fc91a8c99511ba73f6fcc5706937943bb62cec92
Teesha <157734859+Teesha-Verma@users.noreply.github.com>
2025-08-19 11:05:43 +0530

    Update modifyVar.java

--- oops/modifyVar.java
+++ oops/modifyVar.java
@@ -1,1 +1,38 @@
+package bridgeLabz.oops;
+class modifyVar{
+int num;
+String alpha;
+
+static String colorname="hello";
+
+modifyVar(int num, String alpha){
+    this.num=num;
+    this.alpha=alpha;
+}
+
+void display(){
+    System.out.println("number: "+num+" , alphabet: "+alpha+" , name : "+colorname);
+
+}
+
+public static void main(String[] args){
+ 
+    modifyVar v1=new modifyVar(23,"w");
+    modifyVar v2=new modifyVar(1,"a");
+    System.out.println("before");
+    v1.display();
+    v2.display();
+
+    v1.colorname="yellow";
+    System.out.println("after");
+    v1.display();
+    v2.display();
+
+
+
+
+
+ }
+}
+
 
