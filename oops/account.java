a222dbbdf513f82e461febf44c1d977ff8befc2a
Teesha <157734859+Teesha-Verma@users.noreply.github.com>
2025-08-19 11:50:24 +0530

    Create account.java

---
+++ oops/account.java
@@ -0,0 +1,12 @@
+package bridgeLabz.oops;
+class account{
+    String accountHolder;
+    double balance;
+
+    public static void main(String[] args){
+        account acc=new account();
+        System.out.println(acc.accountHolder);
+        System.out.println(acc.balance);
+    }
+    
+}
