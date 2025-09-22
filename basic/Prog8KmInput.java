5ca36e9cfca864e9f6dfeb17f79761b75deb3958
Teesha-Verma <teeshaverma1@gmail.com>
2025-09-22 11:25:09 +0530

    files added

--- basic/Prog8KmInput.java
+++ basic/Prog8KmInput.java
@@ -7,5 +7,6 @@ public class Prog8KmInput {
         double km = input.nextDouble();
         double miles = km / 1.6;
         System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
+        input.close();
     }
 }
