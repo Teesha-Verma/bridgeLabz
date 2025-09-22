648b186cc7735d7c3b885b7ab616680ac41820b6
Teesha-Verma <teeshaverma1@gmail.com>
2025-09-22 10:58:55 +0530

    files added

---
+++ basic2/OddEven.java
@@ -0,0 +1,19 @@
+package bridgeLabz.basic2;
+import java.util.Scanner;
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int number = sc.nextInt();
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer");
+            return;
+        }
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0)
+                System.out.println(i + " is even");
+            else
+                System.out.println(i + " is odd");
+        }
+    }
+}
